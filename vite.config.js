57d5151f9b3bf55929ae62fc0de087594ad98ed2
kawayt <N2420229@ngo.ohara.ac.jp>
2026-01-19 09:46:55 +0900

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,45 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+        injectRegister: 'auto',
+        manifest: {
+            name: 'OneStamp',
+            short_name: 'OneStamp', 
+            description: 'スタンプ送受信アプリ',
+            theme_color: '#ffffff',
+            icons: [
+            {
+                src: 'web-app-manifest-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'web-app-manifest-512x512.png',
+                sizes: '512x512',
+                type: 'image/png'
+            },
+            {
+                src: 'web-app-manifest-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'web-app-manifest-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
+            ]
+        }
+    })],
     server: {
         port: 5173,
         proxy: {
